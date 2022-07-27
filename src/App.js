62b34d14453f01f8627eba453fe46a6245463238
kobby08 <linusquam@gmail.com>
2022-07-27 17:53:36 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -65,7 +65,9 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
               <p>Wind Speed</p>
             </div>
           </div>
